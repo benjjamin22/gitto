1a73557a2f0d3d73fc647a96d1f0ac1a27c71af1
benjjamin22 <122483173+benjjamin22@users.noreply.github.com>
2024-04-12 19:34:10 +0100

    Update script.js

--- public/mysa/nuaa/script.js
+++ public/mysa/nuaa/script.js
@@ -23,7 +23,7 @@ async function getData() {
         <div class="movie">
         <img src="${user.picturepath}">
         <div class="movie-info">
-      <h3>>${user.Aname.Name} ${user.Aname.Mname} ${user.Aname.Surname} </h3>
+      <h3>>${user.Aname.Name}    ${user.Aname.Mname}   ${user.Aname.Surname} </h3>
       <span>${user.Validity}</span>
         </div></div> </a>
         `
