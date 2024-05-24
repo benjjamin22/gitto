0a42feb3aa8c55d7cf1d8d87a0eec5df052ce3ca
benjamin <deeprising4ilife@gmail.com>
2024-05-24 06:49:23 -0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
     //const http = require('http')
-    //const cron = require('node-cron');
+const cron = require('node-cron');
+const axios = require('axios');
 
 //const rateLimit = require('express-rate-limit')
 require('dotenv').config()
@@ -13,22 +14,34 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 
+const serverUrl = 'https://mymongoose.onrender.com';
+
+const keepAlive = () => {
+    axios.get(serverUrl)
+        .then(response => {
+            console.log(`server response with status:${response.status}`)
+        })
+        .catch(error => {
+            console.log(`error keeping server alive:${error.message}`)
+        })
+}
+
 
 //function keepServerAwaike() {
-//http.get('https://mymongoose.onrender.com', (res) => {
+//axios.get('https://mymongoose.onrender.com', (res) => {
 // console.log(`Status Code: ${res.statusCode}`);
 //}).on('error', (e) => {
 //console.error(`Error: ${e.message}`);
 //});
 //}
 
-// Schedule the task to run every 5 minutes
-//cron.schedule('*/14 * * * *', () => {
-//  console.log('Sending keep-alive request to server...');
-// keepServerAwaike();
-//});
+//Schedule the task to run every 5 minutes
+cron.schedule('*/14 * * * *', () => {
+    console.log('Sending keep-alive request to server...');
+    keepAlive();
+});
 
-//console.log('Keep-alive script started.');
+console.log('Keep-alive script started.');
 
 // Rate limiting
 //const limiter = rateLimit({
