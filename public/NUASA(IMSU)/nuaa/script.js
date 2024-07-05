f742cc270a72d68b6e9cad1316d0e0662a5d1f5f
benjamin <deeprising4ilife@gmail.com>
2024-07-05 11:34:54 -0800

    first commit

--- public/NUASA(IMSU)/nuaa/script.js
+++ public/NUASA(IMSU)/nuaa/script.js
@@ -98,7 +98,7 @@ async function getmovie() {
                             <div style="width:25%;margin:0 1px;">
                                 <h1 style="font-size:12px;margin-top:-5px;text-align:center;padding:0 1.5rem;">B/G</h1>
                                 <div class="profile-info">
-                                <h1 style="color:black;padding:0 .8rem;">-${user.Bloodgroup}-</h1>
+                                <h1 style="color:black;padding:0 .8rem;">${user.Bloodgroup}</h1>
                                 </div>
                             </div>
                             <div style="width:45%;margin:0 1px;">
