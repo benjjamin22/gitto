9dd19e51ce2ee52940585882d591965e3c33d424
benjamin <deeprising4ilife@gmail.com>
2024-07-04 16:31:48 -0800

    first commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.set('trust proxy', 1)
 app.use(cors())
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'mypublic')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', require('./routes'))
