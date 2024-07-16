6744055b4ff00850138bf1bdaecae369fc9a740a
benjamin <deeprising4ilife@gmail.com>
2024-07-15 16:43:11 -0800

    first commit

--- public/NUASA(IMSU)/nuaa/script.js
+++ public/NUASA(IMSU)/nuaa/script.js
@@ -95,18 +95,22 @@ async function getmovie() {
                     </div>
                     <div class="profile-bottom">
                         <div style="display:flex;">
-                           
-                            <div style="width:48%;margin:0 1px;">
-                            <h1 style="font-size:12px;margin-top:-5px;text-align:center;padding:0 1rem;">STATUS/VALIDITY</h1>
-                                <div style="flex-direction:column;"class="profile-info">
-                                <h1 style="color:black;padding:0 .8rem;margin-top:-5px;">MEMBER</h1>
-                                <h1 style="margin:0px;color:red;font-size:12px;margin-bottom:0px;line-height:.5rem;">- ${user.Validity} -</h1>
+                            <div style="width:31%;margin:0 1px;">
+                                <h1 style="font-size:12px;margin-top:-5px;text-align:center;padding:0 1.5rem;">VALIDITY</h1>
+                                <div class="profile-info">
+                                <h1 style="color:black;padding:0 .8rem;margin-top:-5px;">2028</h1>
                                 </div>
                             </div>
-                            <div style="width:48%;margin:0 1px;">
-                                <h1 style="font-size:12px;margin-top:-5px;text-align:center;padding:0 1.5rem;">GENDER</h1>
+                            <div style="width:31%;margin:0 1px;">
+                            <h1 style="font-size:12px;margin-top:-5px;text-align:center;padding:0 1rem;">STATUS</h1>
+                                <div style="flex-direction:column;"class="profile-info">
+                                <h1 style="color:black;padding:0 .8rem;margin-top:-5px;">MEMBER</h1>
+                                </div>
+                            </div>
+                            <div style="width:31%;margin:0 1px;">
+                                <h1 style="font-size:12px;margin-top:-5px;text-align:center;padding:0 1rem;">GENDER</h1>
                                 <div class="profile-info">
-                                <h1 style="color:black;padding:0 .8rem;">${user.Sex}</h1>
+                                <h1 style="color:black;padding:0 .8rem;margin-top:-5px;">${user.Sex}</h1>
                                 </div>
                             </div>
                         </div>
