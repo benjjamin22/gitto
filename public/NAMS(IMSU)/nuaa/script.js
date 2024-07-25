4f031f2702d17c0f7825355a2b1ef0597d5d546e
benjamin <deeprising4ilife@gmail.com>
2024-07-24 20:08:47 -0800

    first commit

--- public/NAMS(IMSU)/nuaa/script.js
+++ public/NAMS(IMSU)/nuaa/script.js
@@ -23,7 +23,7 @@ async function getData() {
         <div class="movie">
         <img src="${user.picturepath}">
         <div class="movie-info">
-      <h3>${user.Fullname}</h3>
+      <h3>${user.Name}</h3>
       <span>${user.Phoneno1}</span>
         </div></div> 
         `
